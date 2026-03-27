762eec2096d58af761d2dc4449b92877d8f64a0d
MiladRSL <milad.rasooli@gmail.com>
2026-03-27 13:54:53 -0400

    change secure to false for smtp

--- src/services/mailer/mailer.class.ts
+++ src/services/mailer/mailer.class.ts
@@ -1,61 +1,71 @@
-const nodemailer = require('nodemailer')
+const nodemailer = require("nodemailer");
 
-const MAIL_SERVICE = process.env.MAIL_SERVICE
-const USERNAME = process.env.SMTP_USERNAME
-const PASSWORD = process.env.SMTP_PASSWORD
-const HOST = process.env.SMTP_HOST
-const PORT = process.env.SMTP_PORT
+const MAIL_SERVICE = process.env.MAIL_SERVICE;
+const USERNAME = process.env.SMTP_USERNAME;
+const PASSWORD = process.env.SMTP_PASSWORD;
+const HOST = process.env.SMTP_HOST;
+const PORT = process.env.SMTP_PORT;
 class ConsoleMailer {
   sendMail(data: Object) {
-    console.log('Sending email to console.log:', JSON.stringify(data, null, 2))
+    console.log("Sending email to console.log:", JSON.stringify(data, null, 2));
   }
 }
 
 export class Mailer {
-  mailer: any
+  mailer: any;
 
   constructor() {
-    this.mailer = null
+    this.mailer = null;
   }
 
   setup() {
     switch (MAIL_SERVICE) {
-      case 'uhn':
+      case "uhn":
         this.mailer = nodemailer.createTransport(
           // To bypass LEAF SIG error include rejectUnauthroized and set secure to false
           {
-            host: 'mailhub.uhnresearch.ca',
+            host: "mailhub.uhnresearch.ca",
             port: 25,
             tls: { rejectUnauthorized: false },
             secure: false,
           },
-        )
-        break
+        );
+        break;
 
-      case 'mailhog':
-        this.mailer = nodemailer.createTransport({ host: 'mailhog', port: 1025, secure: false })
-        break
-      case 'smtp':
+      case "mailhog":
+        this.mailer = nodemailer.createTransport({
+          host: "mailhog",
+          port: 1025,
+          secure: false,
+        });
+        break;
+      case "smtp":
         this.mailer = nodemailer.createTransport({
           host: HOST,
           port: PORT,
-          secure: true,
+          secure: false,
           debug: true,
           logger: true,
           auth: { user: USERNAME, pass: PASSWORD },
-        })
-        break
+        });
+        break;
 
       default:
-        this.mailer = new ConsoleMailer()
+        this.mailer = new ConsoleMailer();
     }
   }
 
-  create(data: { from: string; to: string; subject: string; text: string; list?: Object }) {
+  create(data: {
+    from: string;
+    to: string;
+    subject: string;
+    text: string;
+    list?: Object;
+  }) {
     if (!this.mailer) {
-      this.setup()
+      this.setup();
     }
 
-    return this.mailer.sendMail(data)
+    return this.mailer.sendMail(data);
   }
 }
