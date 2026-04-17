700e237e056f7bc8222929fabeb16f91962933b2
MiladRSL <milad.rasooli@gmail.com>
2026-04-17 11:28:19 -0400

    fix: wire up config and env

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import 'dotenv/config'
 import app from './app'
 import logger from './logger'
 
-const port = app.get('port')
-const host = app.get('host')
+const port = Number(process.env.PORT ?? app.get("port"));
+const host = app.get("host");
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason))
