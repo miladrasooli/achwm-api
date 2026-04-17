6677aaff84429ddcadfd8e793501ef27a9a5e50f
MiladRSL <milad.rasooli@gmail.com>
2026-04-17 16:58:10 -0400

    fix: add port configuration to database settings and update paths in default config

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -1,40 +1,41 @@
-import { Sequelize } from 'sequelize'
+import { Sequelize } from "sequelize";
 
-import { Application } from './declarations'
+import { Application } from "./declarations";
 
-export default function(app: Application) {
-  const { database, username, password, host } = app.get('postgres')
+export default function (app: Application) {
+  const { database, username, password, host, port } = app.get("postgres");
 
   const sequelize = new Sequelize(database, username, password, {
     host,
-    dialect: 'postgres',
+    port: Number(port),
+    dialect: "postgres",
     logging: false,
     define: {
       freezeTableName: true,
       timestamps: true,
       underscored: true,
-      createdAt: 'created_at',
-      updatedAt: 'updated_at'
-    }
-  })
-  const oldSetup = app.setup
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  });
+  const oldSetup = app.setup;
 
-  app.set('sequelizeClient', sequelize)
+  app.set("sequelizeClient", sequelize);
 
-  app.setup = function(...args) {
-    const result = oldSetup.apply(this, args)
+  app.setup = function (...args) {
+    const result = oldSetup.apply(this, args);
 
     // Set up data relationships
-    const models = <any> sequelize.models
-    Object.keys(models).forEach(name => {
-      if ('associate' in models[name]) {
-        models[name].associate(models)
+    const models = <any>sequelize.models;
+    Object.keys(models).forEach((name) => {
+      if ("associate" in models[name]) {
+        models[name].associate(models);
       }
-    })
+    });
 
     // Sync to the database
-    app.set('sequelizeSync', sequelize.sync())
+    app.set("sequelizeSync", sequelize.sync());
 
-    return result
-  }
+    return result;
+  };
 }
